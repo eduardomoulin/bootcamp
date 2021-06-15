0b530759c6be268b11097346afd1ced9aada0682
Leomar Moulin <eduets2@gmail.com>
2021-06-15 20:36:53 -0300

    criação de repositorio com o codigo do bootcamp

---
+++ src/main/java/com/project/bootcamp/model/dto/StockDTO.java
@@ -0,0 +1,69 @@
+package com.project.bootcamp.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+public class StockDTO {
+
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @DecimalMin(value = "0.00")
+    @Digits(integer = 6, fraction = 2) //Aceita no maximo 6 com dois apos a virgula
+    private Double price;
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate date;
+
+    @NotNull
+    @Digits(integer = 3, fraction = 2)
+    private Double Variation;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Double getVariation() {
+        return Variation;
+    }
+
+    public void setVariation(Double variation) {
+        Variation = variation;
+    }
+}
